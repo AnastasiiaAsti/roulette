dedd2038a98041c1ce4063e8b183bd0348e78ec6
Asti <astistateofmind@gmail.com>
2023-01-03 16:44:21 -0800

    extract value from the betting board

--- main.js
+++ main.js
@@ -13,13 +13,16 @@ let numRed = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 let wheelnumbers = [0, 32, 15, 19, 5, 21, 2, 25, 17, 34, 6, 27, 14, 36, 11, 30, 8, 23, 10, 5 ,24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26];
 
 let betNumber = []
+let boardNumber = []
 
 /*----- event listeners -----*/
 BETTING_BOARD.addEventListener('click', handleClick);
 SPIN_BTN.addEventListener('click', spin);
 
 function handleClick(evt) {
-    console.log(evt.target)
+    let cell = evt.target;
+    let value = cell.getAttribute('value')
+    console.log(value)
 }
 
 function handleSpinClick() {
@@ -54,8 +57,8 @@ function spin() {
     setTimeout(function () {
         console.log(winningSpin)
         WINNING_NUMBER.innerHTML = winningSpin
+        betNumber.push(winningSpin)
+        console.log(betNumber)
     }, 1000);
 }
 
-
-console.log(WHEEL_NUMS)
