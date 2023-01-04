75c1f21545cf9cf196b9639c80676587f5ac7bad
Asti <astistateofmind@gmail.com>
2023-01-03 16:30:35 -0800

    change the board to the js version

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ const BALL_TRACK = document.getElementsByClassName('ballTrack');
 const BETTING_BOARD = document.getElementById('board');
 const WINNING_NUMBER = document.getElementById('wheelNumber');
 const SPIN_BTN = document.getElementById('spin_btn');
+const WHEEL_NUMS = document.querySelectorAll('.sect span')
 
 /*----- app's state (variables) -----*/
 
 let numRed = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
-let wheelnumbers = [0, 26, 3, 35, 12, 28, 7, 29, 18, 22, 9, 31, 14, 20, 1, 33, 16, 24, 5, 10, 23, 8, 30, 11, 36, 13, 27, 6, 34, 17, 25, 2, 21, 4, 19, 15, 32];
+let wheelnumbers = [0, 32, 15, 19, 5, 21, 2, 25, 17, 34, 6, 27, 14, 36, 11, 30, 8, 23, 10, 5 ,24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26];
 
 let betNumber = []
 
@@ -27,6 +28,26 @@ function handleSpinClick() {
 
 /*----- functions -----*/
 
+function setWheel() {
+    for (i = 0; i < wheelnumbers.length; i++) {
+        let a = i + 1;
+        let spanClass = (wheelnumbers[i] < 10) ? 'single' : 'double';
+        let sect = document.createElement('div');
+        sect.setAttribute('id', 'sect'+a);
+        sect.setAttribute('class', 'sect');
+        let span = document.createElement('span');
+        span.setAttribute('class', spanClass);
+        span.innerText = wheelnumbers[i];
+        sect.append(span);
+        let block = document.createElement('div');
+        block.setAttribute('class', 'block');
+        sect.append(block);
+        WHEEL.append(sect);
+    }
+}
+setWheel()
+
+
 function spin() {
     let winningSpin = Math.floor(Math.random() * 36)
     // spinWheel(winningSpin);
@@ -35,3 +56,6 @@ function spin() {
         WINNING_NUMBER.innerHTML = winningSpin
     }, 1000);
 }
+
+
+console.log(WHEEL_NUMS)
